0edaac6e2b008ec7bc267e086ee07cd644209e1e
Ulrich Schmidt <admin@deruli.de>
2019-07-30 18:05:58 +0200

    make test compatible with the current PHPUnit version, split test class to two methods

--- src/content/modules/is_proxy/test/IsProxyTest.php
+++ src/content/modules/is_proxy/test/IsProxyTest.php
@@ -3,9 +3,12 @@ class IsProxyTest extends PHPUnit_Framework_TestCase {
 	public function tearDown() {
 		unset ( $_SERVER ["HTTP_FORWARDED_FOR"] );
 	}
-	public function testIsProxy() {
+	
+	public function testIsProxyReturnsFalse(){
+		unset ( $_SERVER ["HTTP_FORWARDED_FOR"] );
 		$this->assertFalse ( is_proxy () );
-		
+	}
+	public function testIsProxyReturnsTrue() {		
 		$_SERVER ["HTTP_FORWARDED_FOR"] = "X-Forwarded-For: 75.141.127.128, 
 210.82.237.219, 164.124.134.65";
 		$this->assertTrue ( is_proxy () );
